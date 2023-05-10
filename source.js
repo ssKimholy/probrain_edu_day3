2b7425771d1e51713c598c98d6ab208f341f4caa
kingOfSchottland <46346267+ssKimholy@users.noreply.github.com>
2023-05-10 21:31:22 +0900

    Update source.js

--- source.js
+++ source.js
@@ -1,27 +1,27 @@
-const favoriteText_1 = document.getElementById("favoriteTitle_1");
-const favoriteText_2 = document.getElementById("favoriteTitle_2");
-const favoriteText_3 = document.getElementById("favoriteTitle_3");
+const fvSection = document.getElementById("favoriteSection");
+const musicSection = document.getElementById("musicSection");
+const mbtiSection = document.getElementById("mbtiSection");
 const favoriteContainer = document.getElementById("favoriteContainer");
 const musicContainer = document.getElementById("music");
 const mbtiContainer = document.getElementById("mbtiContainer");
 
 
-function onClickFavor_1() {
-    turnOnElement(favoriteText_1, favoriteContainer);
-    turnOffElement(favoriteText_2, musicContainer);
-    turnOffElement(favoriteText_3, mbtiContainer);
+function onClickSection_1() {
+    turnOnElement(fvSection, favoriteContainer);
+    turnOffElement(musicSection, musicContainer);
+    turnOffElement(mbtiSection, mbtiContainer);
 }
 
-function onClickFavor_2() {
-    turnOnElement(favoriteText_2, musicContainer);
-    turnOffElement(favoriteText_1, favoriteContainer);
-    turnOffElement(favoriteText_3, mbtiContainer);
+function onClickSection_2() {
+    turnOnElement(musicSection, musicContainer);
+    turnOffElement(fvSection, favoriteContainer);
+    turnOffElement(mbtiSection, mbtiContainer);
 }
 
-function onClickFavor_3() {
-    turnOnElement(favoriteText_3, mbtiContainer);
-    turnOffElement(favoriteText_1, favoriteContainer);
-    turnOffElement(favoriteText_2, musicContainer);
+function onClickSection_3() {
+    turnOnElement(mbtiSection, mbtiContainer);
+    turnOffElement(fvSection, favoriteContainer);
+    turnOffElement(musicSection, musicContainer);
 }
 
 function turnOnElement(text, view) {
@@ -36,21 +36,20 @@ function turnOffElement(text, dis_view) {
     dis_view.style.display = "none";
 }
 
-function onHoverFavor(event) {
+function onHoverSection(event) {
     event.target.style.color = "#FF4646";
 }
 
-function onLeaveFavor(event) {
+function onLeaveSection(event) {
     event.target.style.color = "#FAA8A8";
 }
 
-
-favoriteText_1.addEventListener("click", onClickFavor_1);
-favoriteText_2.addEventListener("click", onClickFavor_2);
-favoriteText_3.addEventListener("click", onClickFavor_3);
-favoriteText_1.addEventListener("mouseover", onHoverFavor);
-favoriteText_2.addEventListener("mouseover", onHoverFavor);
-favoriteText_3.addEventListener("mouseover", onHoverFavor);
-favoriteText_1.addEventListener("mouseleave", onLeaveFavor);
-favoriteText_2.addEventListener("mouseleave", onLeaveFavor);
-favoriteText_3.addEventListener("mouseleave", onLeaveFavor);
+fvSection.addEventListener("click", onClickSection_1);
+musicSection.addEventListener("click", onClickSection_2);
+mbtiSection.addEventListener("click", onClickSection_3);
+fvSection.addEventListener("mouseover", onHoverSection);
+musicSection.addEventListener("mouseover", onHoverSection);
+mbtiSection.addEventListener("mouseover", onHoverSection);
+fvSection.addEventListener("mouseleave", onLeaveSection);
+musicSection.addEventListener("mouseleave", onLeaveSection);
+mbtiSection.addEventListener("mouseleave", onLeaveSection);
